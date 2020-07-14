af9cd034854d9ff7963836de66e0dc6ee1eeb940
Divine <48183131+divine@users.noreply.github.com>
2020-07-14 17:16:04 +0300

    Initial commit

---
+++ pages/_slug.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="container">
+    <div>
+      <h1>There should be a title here: {{ myData.title }}</h1>
+      <p>There should be some text below:
+        <br>
+        {{ myData.text }}
+      </p>
+      <client-only>
+        <nuxt-link to="/fr/test">Go to nested slug</nuxt-link>
+      </client-only>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { myData: { title: "", text: "" } };
+  },
+  fetch() {
+    this.myData.title = "Fetched a title!";
+    this.myData.text = "Fetch a text!";
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+h1 {
+  font-weight: 400;
+}
+.name {
+  color: #00C48D;
+}
+p {
+  margin-top: 20px;
+}
+</style>
---
+++ pages/nested/_slug.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <div>
+      <h1>There should be a title here: {{ myData.title }}</h1>
+      <p>There should be some text below:
+        <br>
+        {{ myData.text }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { myData: { title: "", text: "" } };
+  },
+  fetch() {
+    this.myData.title = "Nested - Dynamic subpage!";
+    this.myData.text = "Nested - subpage text!";
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+h1 {
+  font-weight: 400;
+}
+.name {
+  color: #00C48D;
+}
+p {
+  margin-top: 20px;
+}
+</style>
